e53cbca002d79a36c6345cea6be1add34a14fc3d
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-06-20 15:39:11 +0400

    Setup dependency injection.

---
+++ app/src/main/java/com/elvina/cryptoinfo/CryptoInfoApplication.kt
@@ -0,0 +1,8 @@
+package com.elvina.cryptoinfo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoInfoApplication: Application() {
+}
--- app/src/main/java/com/elvina/cryptoinfo/other/Constants.kt
+++ app/src/main/java/com/elvina/cryptoinfo/other/Constants.kt
@@ -1,4 +1,6 @@
 package com.elvina.cryptoinfo.other
 
 object Constants {
+
+    const val BASE_URL: String = "https://api.coinpaprika.com/"
 }
