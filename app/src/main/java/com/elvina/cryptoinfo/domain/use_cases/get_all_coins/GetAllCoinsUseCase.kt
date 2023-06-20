c0cbfd1501b7afc88356f7c6c85a56399cd8b670
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-06-20 12:51:54 +0400

    Create use cases.

---
+++ app/src/main/java/com/elvina/cryptoinfo/domain/use_cases/get_all_coins/GetAllCoinsUseCase.kt
@@ -0,0 +1,28 @@
+package com.elvina.cryptoinfo.domain.use_cases.get_all_coins
+
+import com.elvina.cryptoinfo.data.remote.dto.toCoin
+import com.elvina.cryptoinfo.domain.model.Coin
+import com.elvina.cryptoinfo.domain.repository.CoinRepository
+import com.elvina.cryptoinfo.other.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+import javax.inject.Inject
+
+class GetAllCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow{
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        }catch(e: HttpException){
+            emit(Resource.Error(e.localizedMessage ?: "An error occured"))
+        }catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server. Check internet connection."))
+        }
+    }
+}
---
+++ app/src/main/java/com/elvina/cryptoinfo/domain/use_cases/get_one_coin/GetOneCoinUseCase.kt
@@ -0,0 +1,30 @@
+package com.elvina.cryptoinfo.domain.use_cases.get_one_coin
+
+import com.elvina.cryptoinfo.data.remote.dto.toCoin
+import com.elvina.cryptoinfo.data.remote.dto.toCoinDetail
+import com.elvina.cryptoinfo.domain.model.Coin
+import com.elvina.cryptoinfo.domain.model.CoinDetail
+import com.elvina.cryptoinfo.domain.repository.CoinRepository
+import com.elvina.cryptoinfo.other.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+import javax.inject.Inject
+
+class GetOneCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+){
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow{
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        }catch(e: HttpException){
+            emit(Resource.Error(e.localizedMessage ?: "An error occured"))
+        }catch (e: IOException){
+            emit(Resource.Error("Couldn't reach server. Check internet connection."))
+        }
+    }
+}
