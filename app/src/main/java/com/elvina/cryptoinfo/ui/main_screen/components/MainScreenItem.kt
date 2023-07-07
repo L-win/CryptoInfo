497f3e600025ae414def7931fc294e681b0143c8
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-07-07 22:38:05 +0400

    Use Card view form Main Screen.

--- app/src/main/java/com/elvina/cryptoinfo/ui/main_screen/components/MainScreenItem.kt
+++ app/src/main/java/com/elvina/cryptoinfo/ui/main_screen/components/MainScreenItem.kt
@@ -2,9 +2,12 @@ package com.elvina.cryptoinfo.ui.main_screen.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,22 +25,27 @@ fun MainScreenItem(
     coin: Coin,
     onItemClick: (Coin)->Unit
 ){
-    Row(
-        modifier =  Modifier.fillMaxWidth().clickable {onItemClick(coin)}.padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ){
-        Text(
+    Card(
+        modifier =  Modifier.fillMaxWidth().clickable {onItemClick(coin)}.padding(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
             text = "${coin.rank}. ${coin.name} ${coin.symbol}",
             style = MaterialTheme.typography.bodyMedium,
             overflow = TextOverflow.Ellipsis
         )
-        Text(
+            Text(
             text = if(coin.is_active) "active" else "inactive",
             color = if(coin.is_active) Color.Magenta else Color.Red,
             fontStyle = FontStyle.Italic,
             textAlign = TextAlign.End,
             style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.align(CenterVertically)
+//            modifier = Modifier.align(CenterVertically)
         )
+        }
     }
+
 }
